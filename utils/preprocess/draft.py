76a17216a3d1bd41c585e32e73e0617173a397d7
Ben Ozfirat <benjaminozfirat@gmail.com>
2024-09-12 14:49:50 +0200

    Beginning Streamlit

--- utils/preprocess/draft.py
+++ utils/preprocess/draft.py
@@ -7,7 +7,6 @@ def get_head_to_head_stats(df, home_team, away_team):
     # Filter games between home_team and away_team
     head_to_head_games = df[((df['HomeTeam'] == home_team) & (df['AwayTeam'] == away_team)) |
                             ((df['HomeTeam'] == away_team) & (df['AwayTeam'] == home_team))]
-
     num_games = len(head_to_head_games)
     
     # Return default stats if no games found
@@ -19,26 +18,15 @@ def get_head_to_head_stats(df, home_team, away_team):
             'head_to_head_goals_conceded_home_team': 0,
 
             'head_to_head_goals_scored_away_team': 0,
-            'head_to_head_goals_conceded_away_team': 0
-        }
+            'head_to_head_goals_conceded_away_team': 0}
 
     # Calculate statistics for home team
-    home_wins = len(head_to_head_games[(head_to_head_games['HomeTeam'] == home_team) & (head_to_head_games['FTR'] == 'H')]) + \
-                len(head_to_head_games[(head_to_head_games['AwayTeam'] == away_team) & (head_to_head_games['FTR'] == 'A')])
-    home_draws = len(head_to_head_games[(head_to_head_games['FTR'] == 'D')])
-    home_losses = len(head_to_head_games[(head_to_head_games['HomeTeam'] == away_team) & (head_to_head_games['FTR'] == 'H')]) + \
-                  len(head_to_head_games[(head_to_head_games['AwayTeam'] == home_team) & (head_to_head_games['FTR'] == 'A')])
     home_goals_scored = head_to_head_games[head_to_head_games['HomeTeam'] == home_team]['FTHG'].sum() + \
                         head_to_head_games[head_to_head_games['AwayTeam'] == home_team]['FTAG'].sum()
     home_goals_conceded = head_to_head_games[head_to_head_games['HomeTeam'] == home_team]['FTAG'].sum() + \
                           head_to_head_games[head_to_head_games['AwayTeam'] == home_team]['FTHG'].sum()
 
     # Calculate statistics for away team
-    away_wins = len(head_to_head_games[(head_to_head_games['HomeTeam'] == away_team) & (head_to_head_games['FTR'] == 'H')]) + \
-                len(head_to_head_games[(head_to_head_games['AwayTeam'] == home_team) & (head_to_head_games['FTR'] == 'A')])
-    away_draws = len(head_to_head_games[(head_to_head_games['FTR'] == 'D')])
-    away_losses = len(head_to_head_games[(head_to_head_games['HomeTeam'] == home_team) & (head_to_head_games['FTR'] == 'H')]) + \
-                  len(head_to_head_games[(head_to_head_games['AwayTeam'] == away_team) & (head_to_head_games['FTR'] == 'A')])
     away_goals_scored = head_to_head_games[head_to_head_games['HomeTeam'] == away_team]['FTHG'].sum() + \
                         head_to_head_games[head_to_head_games['AwayTeam'] == away_team]['FTAG'].sum()
     away_goals_conceded = head_to_head_games[head_to_head_games['HomeTeam'] == away_team]['FTAG'].sum() + \
@@ -46,13 +34,10 @@ def get_head_to_head_stats(df, home_team, away_team):
 
     return {
         'head_to_head_games': num_games,
-
         'head_to_head_goals_scored_home_team': home_goals_scored,
         'head_to_head_goals_conceded_home_team': home_goals_conceded,
-
         'head_to_head_goals_scored_away_team': away_goals_scored,
-        'head_to_head_goals_conceded_away_team': away_goals_conceded
-    }
+        'head_to_head_goals_conceded_away_team': away_goals_conceded}
 
 
 def get_stats(df, teams, team_status, num_games):
@@ -60,9 +45,7 @@ def get_stats(df, teams, team_status, num_games):
         team_stats = {
             str(team_status)+'_average_goals_scored_'+ str(num_games): 0,
             str(team_status)+'_average_goals_conceded_'+ str(num_games): 0,
-            str(team_status)+'_average_goal_difference_'+ str(num_games): 0,
-
-}
+            str(team_status)+'_average_goal_difference_'+ str(num_games): 0}
  
         # Fetch last num_games where team played as home or away
         team_games = df[(df['HomeTeam'] == team) | (df['AwayTeam'] == team)].head(num_games)
@@ -75,7 +58,6 @@ def get_stats(df, teams, team_status, num_games):
         team_stats[str(team_status)+'_average_goals_scored_'+ str(num_games)] = (home_games['FTHG'].sum() + away_games['FTAG'].sum()) / num_games
         team_stats[str(team_status)+'_average_goals_conceded_'+ str(num_games)] = (home_games['FTAG'].sum() + away_games['FTHG'].sum()) / num_games
         team_stats[str(team_status)+'_average_goal_difference_'+ str(num_games)] = team_stats[str(team_status)+'_average_goals_scored_'+ str(num_games)] - team_stats[str(team_status)+'_average_goals_conceded_'+ str(num_games)]
-        
 
     return team_stats
 
